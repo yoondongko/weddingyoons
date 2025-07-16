d70a778f5a7c42cfec3e34caa45300a15f067280
Ynsoyn <ynsoyn@gmail.com>
2025-07-16 19:22:10 +0900

    거의 다해감 팝업 진행중

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function openWeb(url) {
   window.open(url, "_blank");
 }
 
-function clickbutton(){
-
+function showPopupGroomBank() {
+  document.getElementById("popupGroomBank").style.display = "block";
+}
+function hidePopupGroomBank() {
+  document.getElementById("popupGroomBank").style.display = "none";
 }
