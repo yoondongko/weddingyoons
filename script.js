cd44c2d5fadf0cc3db1401028dfb1915dcb5c831
Ynsoyn <ynsoyn@gmail.com>
2025-07-17 00:16:06 +0900

    계좌번호만 수정하면 됨! 다끝남

--- script.js
+++ script.js
@@ -22,3 +22,41 @@ function showPopupBrideBank() {
 function hidePopupBrideBank() {
   document.getElementById("popupBrideBank").style.display = "none";
 }
+
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      alert("복사 완료: " + text);
+    })
+    .catch(err => {
+      alert("복사 실패: " + err);
+    });
+}
+
+
+let currentIndex = 1;
+const totalImages = 15;
+
+function popupGallery(n) {
+  currentIndex = n;
+  showImage(currentIndex);
+  document.getElementById("popGallery").style.display = "block";
+}
+
+function closeGallery() {
+  document.getElementById("popGallery").style.display = "none";
+}
+
+function showImage(index) {
+  document.getElementById("galleryModalImage").src = `gallery_${index}.jpg`;
+}
+
+function nextImage() {
+  currentIndex = (currentIndex % totalImages) + 1;
+  showImage(currentIndex);
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 2 + totalImages) % totalImages + 1;
+  showImage(currentIndex);
+}
